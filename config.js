ab917d597758e4ad987451c8533d42828a93ab76
Matt Bankston <matthewbankston@gmail.com>
2015-10-18 19:00:01 -0500

    First commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key:  process.env.CONS_KEY,
+    consumer_secret: process.env.CONS_SECRET,
+    access_token_key: process.env.ACCESS_KEY,
+    access_token_secret: process.env.ACCESS_SECRET
+  }
+};
